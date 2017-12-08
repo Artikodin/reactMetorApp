099c86b28ae1e48d21cad22667fd54b79470ba88
Artikodin <artium.wasse@gmail.com>
2017-12-08 18:09:11 +0100

    correction des lien

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from '/imports/ui/App';
 import NotFound from '/imports/ui/NotFound';
 import Login from '/imports/ui/Login';
-import createBrowserHistory from 'history/createBrowserHistory';
 
 import 'antd/dist/antd.css';
 
@@ -13,23 +13,25 @@ import 'antd/dist/antd.css';
 // });
 
 const authenticate = (nextState, replace) => {
-    if (!Meteor.loggingIn() && !Meteor.userId()) {
-        replace({
-            pathname: '/login',
-            state: { nextPathname: nextState.location.pathname },
-        });
-    }
+  if (!Meteor.loggingIn() && !Meteor.userId()) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
 };
 
 Meteor.startup(() => {
-    render(
-        <Router history={createBrowserHistory()}>
-            <div>
-                <Route exact path="/" component={App} />
-                <Route name="chat" path="/chat/:_id" component={App} onEnter={authenticate} />
-                <Route name="login" path="/login" component={ Login } />
-                <Route path="*" component={ NotFound } />
-            </div>
-        </Router>,
-        document.getElementById('app'));
+  render(
+    <Router history={createBrowserHistory()}>
+      <div>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route name="chat" path="/chat/:_id" component={App} onEnter={authenticate} />
+          <Route name="login" path="/login" component={Login} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>,
+    document.getElementById('app'));
 });
