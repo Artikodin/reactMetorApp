8158735e1a5321a0750c9f808c0df4627a855534
Artikodin <artium.wasse@gmail.com>
2017-12-10 19:50:59 +0100

    petit style de la page login

--- imports/ui/Login.jsx
+++ imports/ui/Login.jsx
@@ -4,11 +4,18 @@ import HeaderBar from '/imports/ui/app/HeaderBar';
 
 // App component - represents the whole app
 export default class Login extends Component {
-
+    handleClick = () => {
+        Session.set('Meteor.loginButtons.dropdownVisible' , true);
+    }
     render() {
         return (
             <div className="appContainer">
                 <HeaderBar/>
+                <div style={{margin:'auto', textAlign:'center'}}>
+                    Vous êtes acctuellement pas connecté au Super Chat. Veuillez vous connecter pour pouvoir commencer à tchatter!
+                    <br/>
+                    <button onClick={this.handleClick}>Sigin in</button>
+                </div>
             </div>
         );
     }
